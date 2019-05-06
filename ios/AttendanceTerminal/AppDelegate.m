a6637b4a3ee5548d80c35a914d0dc4a9b3103a12
reza-abdollahi <abdollahi.reza88@gmail.com>
2019-05-06 16:27:30 +0430

    add splash screen (#1)
    - providing splash screen for both Android and iOS
    - prevent white screen flash between splash screen and main application View (iOS)

--- ios/AttendanceTerminal/AppDelegate.m
+++ ios/AttendanceTerminal/AppDelegate.m
@@ -31,6 +31,12 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+
+  // to prevent a white screen flash between splash screen and the display of root application view
+  UIView* launchScreenView = [[[NSBundle mainBundle] loadNibNamed:@"LaunchScreen" owner:self options:nil] objectAtIndex:0];
+  launchScreenView.frame = self.window.bounds;
+  rootView.loadingView = launchScreenView;
+
   return YES;
 }
 
